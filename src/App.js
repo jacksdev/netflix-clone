7497e3e4ce171bf61c586879798720224e164d1d
abc <jacksdevwork@gmail.com>
2021-03-05 15:39:37 -0800

    set up HomeScreen component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import HomeScreen from './components/HomeScreen';
 
 function App() {
   return (
-    <div className="App">
-    
+    <div className="app">
+      <HomeScreen />
     </div>
   );
 }
---
+++ src/components/HomeScreen.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components';
+
+function HomeScreen() {
+    return (
+        <HomeScreenContainer>
+            Home screen
+        </HomeScreenContainer>
+    )
+}
+
+export default HomeScreen
+
+
+const HomeScreenContainer = styled.div`
+    display:flex;
+`
