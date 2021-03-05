68dc3a8acd605bdad459d1b9c5408d0b54f50dea
abc <jacksdevwork@gmail.com>
2021-03-05 15:10:17 -0800

    set up firebase config

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDWte54o9EAd1Ra_C0_ZBZcMAOrMn5j76M",
+    authDomain: "netflix-test-86ae6.firebaseapp.com",
+    projectId: "netflix-test-86ae6",
+    storageBucket: "netflix-test-86ae6.appspot.com",
+    messagingSenderId: "599599359959",
+    appId: "1:599599359959:web:304563d36c93c4aee0cd92"
+  };
